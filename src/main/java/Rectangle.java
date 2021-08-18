4a27807dec412d11447afe5567cdc6d712277e3c
Sayeddilawar <sayeddilawar631@gmail.com>
2021-08-18 15:51:08 +0530

    Requirement 1 : Implemented Area of Rectangle.

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,2 +1,15 @@
-public class Rectangle {
+public class Rectangle
+{
+    int length;
+    int width;
+
+    public Rectangle(int length,int width)
+    {
+        this.length=length;
+        this.width = width;
+    }
+
+    public int area() {
+        return length*width;
+    }
 }
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,2 +1,42 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RectangleTest {
+
+    @Test
+    void toCalculateAreaOfRectangleWithPositiveDimensions()
+    {
+        Rectangle object= new Rectangle(6,4);
+        int expectedArea = 24;
+        int actualArea = object.area();
+        assertEquals(expectedArea,actualArea);
+    }
+
+    @Test
+    void toCalculateAreaOfRectangleWithPositiveLengthNegativeWidth()
+    {
+        Rectangle object = new Rectangle(6,-4);
+        int expectedArea = -24;
+        int actualArea = object.area();
+        assertEquals(expectedArea,actualArea);
+    }
+
+    @Test
+    void toCalculateAreaOfRectangleWithNegativeLengthPositiveWidth()
+    {
+        Rectangle object = new Rectangle(-6,4);
+        int expectedArea = -24;
+        int actualArea = object.area();
+        assertEquals(expectedArea,actualArea);
+    }
+
+    @Test
+    void toCalculateAreaOfRectangleWithNegativeDimensions()
+    {
+        Rectangle object = new Rectangle(-6,-4);
+        int expectedArea = 24;
+        int actualArea = object.area();
+        assertEquals(expectedArea,actualArea);
+    }
+
+
 }
